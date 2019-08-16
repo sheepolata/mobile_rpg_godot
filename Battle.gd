427c5e5715c0c26cf284fac8e005bbbe554c11f6
Antoine Gaget <antoine.gaget@gmail.com>
2019-08-16 18:07:50 +0100

    Tutorial ep.8 done
    https://www.youtube.com/watch?v=KSi35Gh6SL0

--- Battle.gd
+++ Battle.gd
@@ -2,15 +2,24 @@ extends Node
 
 const BattleUnits = preload("res://BattleUnits.tres")
 
+export(Array, PackedScene) var enemies = []
+
 onready var battle_actions_button = $UI/BattleActionsButtons
+onready var anim = $AnimationPlayer
+onready var next_room_button = $UI/NextRoomButtonContainer
+onready var enemy_position = $EnemyPosition
 
 func _ready():
+	randomize()
 	start_player_turn()
+	var enemy = BattleUnits.enemy
+	if enemy != null:
+		enemy.connect("death", self, "_on_enemy_death")
 
 func start_enemy_turn():
 	battle_actions_button.hide()
 	var enemy = BattleUnits.enemy
-	if enemy != null:
+	if enemy != null and !enemy.is_dead():
 		enemy.attack()
 		yield(enemy, "end_turn")
 	start_player_turn()
@@ -22,5 +31,24 @@ func start_player_turn():
 	yield(player_stats, "end_turn")
 	start_enemy_turn()
 
-func _on_Enemy_death():
+func create_new_enemy():
+	enemies.shuffle()
+	var Enemy = enemies.front()
+	var enemy = Enemy.instance()
+	enemy_position.add_child(enemy)
+	enemy.connect("death", self, "_on_enemy_death")
+	
+func _on_enemy_death():	
+	next_room_button.show()
 	battle_actions_button.hide()
+
+func _on_NextRoomButton_pressed():
+	next_room_button.hide()
+	anim.play("fade_to_new_room")
+	yield(anim, "animation_finished")
+	
+	var player_stats = BattleUnits.player_stats
+	player_stats.ap = player_stats.max_ap
+	
+	battle_actions_button.show()
+	create_new_enemy()
--- Enemy.gd
+++ Enemy.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 const BattleUnits = preload("res://BattleUnits.tres")
 
-var hp = 25 setget set_hp
+export(int) var hp = 25 setget set_hp
+export(int) var damage = 3
 
 onready var hp_lab = $HPLabel
 onready var anim_player = $AnimationPlayer
@@ -12,13 +13,15 @@ signal end_turn
 
 func _ready():
 	BattleUnits.enemy = self
+	hp_lab.text = str(hp) + "hp"
 	
 func _exit_tree():
 	BattleUnits.enemy = null
 
 func set_hp(new_hp):
 	hp = new_hp
-	hp_lab.text = str(hp) + "hp"
+	if hp_lab != null:
+		hp_lab.text = str(hp) + "hp"
 		
 func attack() -> void:
 	yield(get_tree().create_timer(0.4), "timeout")
@@ -36,8 +39,7 @@ func take_damage(amount):
 
 func deal_damage():
 	if BattleUnits.player_stats != null:
-		BattleUnits.player_stats.hp -= 4
+		BattleUnits.player_stats.hp -= damage
 	
-
 func is_dead():
 	return self.hp <= 0
